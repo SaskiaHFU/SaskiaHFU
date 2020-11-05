0751d4ef689084625fb9bf91bd5e0be2fbcab596
Saskia <saskia.naomi.schellhammer@hs-furtwangen.de>
2020-11-05 11:43:08 +0100

    Validierer Errors behoben

--- Aufgabe5/script.js
+++ Aufgabe5/script.js
@@ -2,19 +2,8 @@
 function a1() {
     let x = "Alles";
     debugger;
-    console.log(x);
-    func1();
-    console.log("Logo!");
-}
-a1();
-function func1() {
-    console.log("Klar?");
-}
-/* Aufgabe 2 */
-function a1() {
-    let x = "Alles";
-    debugger;
     func2();
+    console.log(x);
     func1();
     console.log(x);
     console.log("Logo!");
@@ -26,4 +15,14 @@ function func1() {
 function func2() {
     console.log("Alles Gute!");
 }
+/* Aufgabe 2 */
+function a2() {
+    let i = 9;
+    debugger;
+    do {
+        console.log(i);
+        i = i - 1;
+    } while (i > 0);
+}
+a2();
 //# sourceMappingURL=script.js.map
--- Aufgabe5/script.ts
+++ Aufgabe5/script.ts
@@ -1,25 +1,8 @@
 function a1(): void {
     let x: string = "Alles";
     debugger;
-    console.log(x);
-    func1();
-    console.log("Logo!");
-    
-}
-
-a1();
-
-function func1(): void {
-    console.log("Klar?");
-}
-
-/* Aufgabe 2 */
-
-function a1(): void {
-    let x: string = "Alles";
-    debugger;
     func2();
-    
+    console.log(x);
     func1();
     console.log(x);
     console.log("Logo!");
@@ -35,6 +18,19 @@ function func1(): void {
 function func2(): void {
     console.log("Alles Gute!");
 }
+
+/* Aufgabe 2 */
+
+function a2(): void {
+    let i: number = 9;
+    debugger;
+    do {
+        console.log(i);
+        i = i - 1;
+    } while( i > 0);
+}
+
+a2();
 
 
 
