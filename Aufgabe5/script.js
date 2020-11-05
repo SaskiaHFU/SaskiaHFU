a813713089f2e82f556d3b068bee312e2738bd29
Saskia <saskia.naomi.schellhammer@hs-furtwangen.de>
2020-11-05 11:05:47 +0100

    Aufgabe 5

--- Aufgabe5/script.js
+++ Aufgabe5/script.js
@@ -10,6 +10,19 @@ a1();
 function func1() {
     console.log("Klar?");
 }
+/* Aufgabe 2 */
+function a1() {
+    let x = "Alles";
+    debugger;
+    func2();
+    func1();
+    console.log(x);
+    console.log("Logo!");
+}
+a1();
+function func1() {
+    console.log("Klar?");
+}
 function func2() {
     console.log("Alles Gute!");
 }
--- Aufgabe5/script.ts
+++ Aufgabe5/script.ts
@@ -4,6 +4,26 @@ function a1(): void {
     console.log(x);
     func1();
     console.log("Logo!");
+    
+}
+
+a1();
+
+function func1(): void {
+    console.log("Klar?");
+}
+
+/* Aufgabe 2 */
+
+function a1(): void {
+    let x: string = "Alles";
+    debugger;
+    func2();
+    
+    func1();
+    console.log(x);
+    console.log("Logo!");
+    
 }
 
 a1();
@@ -15,4 +35,7 @@ function func1(): void {
 function func2(): void {
     console.log("Alles Gute!");
 }
+
+
+
 
