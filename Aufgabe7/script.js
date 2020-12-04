e30cd9d053ecc6552cef8204e96df9bc66641b15
Saskia <saskia.naomi.schellhammer@hs-furtwangen.de>
2020-12-04 07:54:06 +0100

    fertig

--- Aufgabe7/script.js
+++ Aufgabe7/script.js
@@ -1,17 +1,16 @@
 "use strict";
-//Bilder anzeigen
-function bilder(_info, _name) {
-    let selectElement = document.getElementById("row");
-    for (let i = 0; i < _info.length; i++) {
+function bilder(_array, _name) {
+    let grabElement = document.getElementById("row");
+    for (let i = 0; i < _array.length; i++) {
         let div = document.createElement("div");
         div.className = ("col-1");
         div.addEventListener("click", select);
-        selectElement.appendChild(div);
-        let optionImage = document.createElement("img");
-        optionImage.src = _info[i].bild;
-        div.dataset.index = i.toString();
+        grabElement.appendChild(div);
+        let img = document.createElement("img");
+        img.src = _array[i].bild;
+        div.dataset.index = _array[i].bild;
         div.dataset.typ = _name;
-        div.appendChild(optionImage);
+        div.appendChild(img);
     }
 }
 // //Auf unterschiedlichen Seiten Bilder laden
@@ -35,26 +34,24 @@ async function load() {
     }
 }
 window.addEventListener("load", load);
+async function auslesen() {
+    let selectElement = document.getElementById("letzte-bilder");
+    let properties = Object.getOwnPropertyNames(obstsalat1);
+    for (let property of properties) {
+        let div = document.createElement("div");
+        div.className = ("col-1");
+        selectElement.appendChild(div);
+        let value = localStorage.getItem(property);
+        let optionImage = document.createElement("img");
+        optionImage.src = value;
+        div.appendChild(optionImage);
+    }
+}
 //Daten speichern
 function select(_event) {
     let target = _event.currentTarget;
     window.localStorage.setItem(target.dataset.typ, target.dataset.index);
     console.log(localStorage.getItem(target.dataset.typ));
-}
-function auslesen() {
-    let selectElement = document.getElementById("letzte-bilder");
-    for (let i = 0; i < localStorage.length; i++) {
-        let div = document.createElement("div");
-        div.className = ("col-1");
-        selectElement.appendChild(div);
-        let schluessel = localStorage.key(i);
-        let value = parseInt(localStorage.getItem(schluessel)); //nummer in string speichern
-        let bild = obstsalat1[schluessel][value];
-        console.log(bild);
-        let optionImage = document.createElement("img");
-        optionImage.src = bild.bild;
-        div.appendChild(optionImage);
-    }
 }
 //Daten löschen
 let endeButton = document.getElementById("ende");
@@ -62,25 +59,21 @@ endeButton.addEventListener("click", loeschen);
 function loeschen(_e) {
     window.localStorage.clear();
 }
-//asynchron
 async function send(_url) {
     let params = new URLSearchParams(localStorage);
     _url = _url + "?" + params.toString();
     let response = await fetch(_url);
-    console.log("Response", await response.json());
-    // Platz generieren
-    let responseServer = document.getElementById("antwort");
-    let messageText = document.createElement("p");
-    let message = response["message"];
+    let serverMessage = await response.json();
+    let serverResult = document.getElementById("serverresult");
+    let text = document.createElement("p");
     //Catch
-    if (message !== undefined) {
-        messageText.innerText = message;
+    if (serverMessage.message !== undefined) {
+        text.innerText = serverMessage.message;
     }
-    let error = response["error"];
-    if (error !== undefined) {
-        messageText.setAttribute("style", "color:red");
-        messageText.innerText = error;
+    if (serverMessage.error !== undefined) {
+        text.setAttribute("style", "color:red");
+        text.innerText = serverMessage.error;
     }
-    responseServer.appendChild(messageText);
+    serverResult.appendChild(text);
 }
 //# sourceMappingURL=script.js.map
--- Aufgabe7/script.ts
+++ Aufgabe7/script.ts
@@ -1,29 +1,23 @@
-//Bilder anzeigen
 
-function bilder(_info: Bild[], _name: string): void {
+function bilder(_array: Bild[], _name: string): void {
 
-    let selectElement: HTMLDivElement = <HTMLDivElement>document.getElementById("row");
+    let grabElement: HTMLDivElement = <HTMLDivElement>document.getElementById("row");
 
-    for (let i: number = 0; i < _info.length; i++) {
+    for (let i: number = 0; i < _array.length; i++) {
         let div: HTMLDivElement = <HTMLDivElement>document.createElement("div");
-
-
         div.className = ("col-1");
 
         div.addEventListener("click", select);
 
-        selectElement.appendChild(div);
+        grabElement.appendChild(div);
 
-        let optionImage: HTMLImageElement = <HTMLImageElement>document.createElement("img");
-        optionImage.src = _info[i].bild;
+        let img: HTMLImageElement = <HTMLImageElement>document.createElement("img");
+        img.src = _array[i].bild;
 
-        div.dataset.index = i.toString();
+        div.dataset.index = _array[i].bild;
         div.dataset.typ = _name;
 
-        div.appendChild(optionImage);
-
-
-
+        div.appendChild(img);
     }
 }
 
@@ -59,7 +53,24 @@ async function load(): Promise<void> {
 
 window.addEventListener("load", load);
 
+async function auslesen(): Promise<void> {
+    let selectElement: HTMLDivElement = <HTMLDivElement>document.getElementById("letzte-bilder");
 
+    let properties: string[] = Object.getOwnPropertyNames(obstsalat1);
+
+    for (let property  of properties ) {
+        let div: HTMLDivElement = <HTMLDivElement>document.createElement("div");
+        div.className = ("col-1");
+        selectElement.appendChild(div);
+        
+        let value: string = localStorage.getItem(property);
+
+        let optionImage: HTMLImageElement = <HTMLImageElement>document.createElement("img");
+        optionImage.src = value;
+
+        div.appendChild(optionImage);
+    }
+}
 //Daten speichern
 
 function select(_event: MouseEvent): void {
@@ -70,30 +81,6 @@ function select(_event: MouseEvent): void {
     console.log(localStorage.getItem(target.dataset.typ));
 
 
-}
-
-function auslesen(): void {
-    let selectElement: HTMLDivElement = <HTMLDivElement>document.getElementById("letzte-bilder");
-
-    for (let i: number = 0; i < localStorage.length; i++) {
-        let div: HTMLDivElement = <HTMLDivElement>document.createElement("div");
-
-        div.className = ("col-1");
-        selectElement.appendChild(div);
-
-
-        let schluessel: string = localStorage.key(i);
-        let value: number = parseInt(localStorage.getItem(schluessel)); //nummer in string speichern
-        let bild: Bild = (<Bild[]>obstsalat1[schluessel])[value];
-
-        console.log(bild);
-
-        let optionImage: HTMLImageElement = <HTMLImageElement>document.createElement("img");
-        optionImage.src = bild.bild;
-
-
-        div.appendChild(optionImage);
-    }
 }
 
 //Daten löschen
@@ -122,53 +109,37 @@ interface Obstsalat {
     toppings: Bild[];
 }
 
-interface MessageServer {
-
-    message: string;
-    error: string;
-}
-
-//asynchron
-
 async function send(_url: string): Promise<void> {
-
-
 
     let params: URLSearchParams = new URLSearchParams(localStorage);
     _url = _url + "?" + params.toString();
 
-
     let response: Response = await fetch(_url);
-    console.log("Response", await response.json());
 
+    let serverMessage: ServerMessage = await response.json();
+    
+    let serverResult: HTMLElement = document.getElementById("serverresult");
 
+    let text: HTMLParagraphElement = document.createElement("p");
 
-    // Platz generieren
-    let responseServer: HTMLDivElement = <HTMLDivElement>document.getElementById("antwort");
-    let messageText: HTMLParagraphElement = <HTMLParagraphElement>document.createElement("p");
+    //Catch
+    if (serverMessage.message !== undefined) {  
+       text.innerText = serverMessage.message;
+    }
 
-//  '   let message: string = response ["message"];'
+    if (serverMessage.error !== undefined) {
+        text.setAttribute("style", "color:red");
+        text.innerText = serverMessage.error;
+     }
 
-    // //Catch
-    // if (MessageServer.message !== undefined) {
-    //     messageText.innerText = message;
+    serverResult.appendChild(text);
+} 
 
-    // }
+interface ServerMessage {
 
-    // // let error: string = response ["error"];
-
-    // if (MessageServer.error !== undefined) {
-    //     messageText.setAttribute("style", "color:red");
-    //     messageText.innerText = error;
-    // }
-
-    // responseServer.appendChild(messageText);
-
-
-
+    message: string;
+    error: string;
 }
-
-
 
 
 
